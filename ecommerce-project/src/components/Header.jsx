c166dfed66a57f77598790a4c74e2a9d39019cc5
Ruhul <itsruhul@proton.me>
2026-01-30 19:36:44 +0530

    completed chapter 6 challenge

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,4 +1,8 @@
 import { NavLink } from 'react-router';
+import mobileLogoWhite from '../assets/images/mobile-logo-white.png';
+import logoWhite from '../assets/images/logo-white.png';
+import cartIcon from '../assets/images/icons/cart-icon.png';
+import searchIcon from '../assets/images/icons/search-icon.png';
 import './Header.css';
 
 export function Header() {
@@ -8,9 +12,9 @@ export function Header() {
       <div className="left-section">
         <NavLink to="/" className="header-link">
           <img className="logo"
-            src="images/logo-white.png" />
+            src={logoWhite} />
           <img className="mobile-logo"
-            src="images/mobile-logo-white.png" />
+            src={mobileLogoWhite} />
         </NavLink>
       </div>
 
@@ -18,7 +22,7 @@ export function Header() {
         <input className="search-bar" type="text" placeholder="Search" />
 
         <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
+          <img className="search-icon" src={searchIcon} />
         </button>
       </div>
 
@@ -29,7 +33,7 @@ export function Header() {
         </NavLink>
 
         <NavLink className="cart-link header-link" to="/checkout">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <img className="cart-icon" src={cartIcon} />
           <div className="cart-quantity">3</div>
           <div className="cart-text">Cart</div>
         </NavLink>
--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,4 +1,5 @@
 import { Header } from '../components/Header';
+import buyAgain from '../assets/images/icons/buy-again.png'
 import './OrdersPage.css';
 
 export function OrdersPage() {
@@ -50,7 +51,7 @@ export function OrdersPage() {
                   Quantity: 1
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={buyAgain} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -78,7 +79,7 @@ export function OrdersPage() {
                   Quantity: 2
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={buyAgain} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -129,7 +130,7 @@ export function OrdersPage() {
                   Quantity: 2
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={buyAgain} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
